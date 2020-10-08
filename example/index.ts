f5eb16329d577642bf9a14af9ba5ad1af453af96
Patrick Charles-Lundaahl <plundaahl@protonmail.com>
2020-10-08 12:40:42 -0700

    Quick refactor to example/index.ts

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,12 @@
 import { run } from './runner';
 import { setupCanvas, resetCanvas } from './util';
-import { createContext, getContext } from '../src/core';
+import * as DittoImGUI from '../src/core';
 import { button, panel, scrollRegion } from './widgets';
 
 const { canvas, context } = setupCanvas();
-createContext(canvas);
+DittoImGUI.createContext(canvas);
 
-const gui = getContext();
+const gui = DittoImGUI.getContext();
 let dummyIds: number[] = [];
 let nextId: number = 0;
 
@@ -14,16 +14,18 @@ function main() {
     resetCanvas(context);
 
     panel.begin('Control Panel', 50, 50, 100, 100);
-    if (button('Increment')) {
+    if (button('Add Button')) {
         dummyIds.push(nextId++);
     }
+    panel.begin('Some Other Panel', 75, 75, 100, 100);
+    panel.end();
     panel.end();
 
     panel.begin('Display Panel', 250, 50, 100, 200);
     scrollRegion.begin('DisplayScroll');
     for (let i = 0; i < dummyIds.length; i++) {
         const id = dummyIds[i];
-        if (button(`Button ${id}`)) {
+        if (button(`Remove ${id}`)) {
             dummyIds = dummyIds.filter(element => element !== id);
         }
     }
