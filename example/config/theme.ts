8f9827e0b475cc294e2c9ced6fbdb5c5904184b0
Patrick Charles-Lundaahl <plundaahl@protonmail.com>
2020-11-30 12:41:21 -0800

    Improve theme definition

--- example/config/theme.ts
+++ example/config/theme.ts
@@ -1,53 +1,97 @@
 import Color from '../../src/lib/Color';
 import { Theme, RegionType, modes, regionTypes } from '../../src/StyledDittoImGui';
 
-const baseColors: { [R in RegionType]: string } = {
-    panel: '#444',
-    titlebar: '#085',
-    controlStd: '#666',
-    controlDanger: '#DDD',
-    controlImportant: '#DDD',
-    editable: '#FFF',
-};
+const baseHighlight = 0.75;
+const baseLowlight = 0.5;
 
-const highlightAmt = 0.75;
-const lowlightAmt = 0.5;
-const detail = '#FFF';
-const bgSelect = '#555';
-const detailSelect = '#FFF';
+const darkMetal = mkTheme('#222', '#FFF');
+const brightMetal = mkTheme('#666', '#FFF');
 
-export const theme: Theme = (() => {
-    let theme: any = {};
-    for (const regionName of regionTypes) {
-        theme[regionName] = {};
+const darkTeal = mkTheme('#085', '#FFF');
+const brightTeal = mkTheme('#5FA', '#000');
 
-        for (const mode of modes) {
-            theme[regionName][mode] = {};
+const editableMetal = withInverseBevel(
+    withBg(
+        withDetail(brightMetal, '#000'), '#FFF'));
 
-            const colorHex = (baseColors as any)[regionName];
-            const color = Color.fromHexString(colorHex);
+export const theme: Theme = {
+    panel: {
+        idle: { ...darkMetal },
+        focused: { ...darkMetal },
+        active: { ...darkMetal },
+        disabled: { ...darkMetal },
+    },
+    titlebar: {
+        idle: { ...darkTeal },
+        focused: { ...brightTeal },
+        active: { ...brightTeal },
+        disabled: { ...darkTeal },
+    },
+    controlStd: {
+        idle: { ...brightMetal },
+        focused: { ...brightMetal },
+        active: { ...withInverseBevel(brightMetal) },
+        disabled: { ...brightMetal },
+    },
+    controlDanger: {
+        idle: { ...brightMetal },
+        focused: { ...brightMetal },
+        active: { ...withInverseBevel(brightMetal) },
+        disabled: { ...brightMetal },
+    },
+    controlImportant: {
+        idle: { ...brightMetal },
+        focused: { ...brightMetal },
+        active: { ...withInverseBevel(brightMetal) },
+        disabled: { ...brightMetal },
+    },
+    editable: {
+        idle: { ...editableMetal },
+        focused: { ...editableMetal },
+        active: { ...editableMetal },
+        disabled: { ...editableMetal },
+    },
+}
 
-            theme[regionName][mode].bg = color.toHexString();
+interface RegionTheme {
+    bg: string,
+    bgHighlight: string,
+    bgLowlight: string,
+    bgSelected: string,
+    detail: string,
+    detailSelected: string,
+}
 
-            theme[regionName][mode].bgHighlight = color
-                .fromHexString(colorHex)
-                .lighten(highlightAmt)
-                .toHexString();
+function mkTheme(
+    bg: string,
+    detail: string,
+    contrast: number = 1,
+): RegionTheme {
+    const highlight = baseHighlight * contrast;
+    const lowlight = baseLowlight * contrast;
 
-            theme[regionName][mode].bgLowlight = color
-                .fromHexString(colorHex)
-                .darken(lowlightAmt)
-                .toHexString();
+    return {
+        bg,
+        bgHighlight: Color.fromHexString(bg).lighten(highlight).toHexString(),
+        bgLowlight: Color.fromHexString(bg).darken(lowlight).toHexString(),
+        bgSelected: bg,
+        detail,
+        detailSelected: detail,
+    };
+}
 
-            theme[regionName][mode].bgSelected = bgSelect;
-            theme[regionName][mode].detail = detail;
-            theme[regionName][mode].detailSelect = detailSelect;
-        }
-    }
+function withInverseBevel(input: RegionTheme): RegionTheme {
+    return {
+        ...input,
+        bgHighlight: input.bgLowlight,
+        bgLowlight: input.bgHighlight,
+    };
+}
 
-    theme.titlebar.idle.detail = '#0FA';
-    theme.titlebar.focused.bg = '#0FA';
-    theme.titlebar.focused.detail = '#000';
+function withBg(input: RegionTheme, bg: string): RegionTheme {
+    return { ...input, bg, bgSelected: bg };
+}
 
-    return theme as unknown as Theme;
-})();
+function withDetail(input: RegionTheme, detail: string): RegionTheme {
+    return { ...input, detail, detailSelected: detail };
+}
