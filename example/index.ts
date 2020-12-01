00e83357058ed87e9ebde97d24215c7a5df70db2
Patrick Charles-Lundaahl <plundaahl@protonmail.com>
2020-12-01 13:29:09 -0800

    Mock out simple color editor

--- example/index.ts
+++ example/index.ts
@@ -3,36 +3,35 @@ import { theme, boxSize, font } from './config';
 import { setupCanvas, resetCanvas } from './util';
 import { StyledDittoContextImpl } from '../src/StyledDittoImGui';
 import { button, panel, slider, editableText } from './widgets';
+import Color from '../src/lib/Color';
 
 const { canvas, context } = setupCanvas();
 const gui = new StyledDittoContextImpl(canvas, theme, boxSize, font);
 
-let numA = 50;
-let numB = 50;
-let mode: string = 'add';
-
-const numABinding = (v = numA) => numA = v;
-const numBBinding = (v = numB) => numB = v;
+const color = new Color();
+const hue = (_ = color.h) => color.h = _;
+const sat = (_ = color.s) => color.s = _;
+const lum = (_ = color.l) => color.l = _;
+const red = (_ = color.r) => color.r = _;
+const grn = (_ = color.g) => color.g = _;
+const blu = (_ = color.b) => color.b = _;
 
 function main() {
     resetCanvas(context);
 
-    panel.begin(gui, 'Control Panel', 50, 50, 400, 400);
-    slider(gui, 'A', numABinding, 0, 100, Math.round);
-    slider(gui, 'B', numBBinding, 0, 100, Math.round);
+    panel.begin(gui, 'Control Panel', 50, 50, 300, 400);
+    slider(gui, 'Hue', hue, 0, 360, Math.round);
+    slider(gui, 'Saturation', sat, 0, 1);
+    slider(gui, 'Luma', lum, 0, 1);
+    slider(gui, 'Red', red, 0, 255, Math.round);
+    slider(gui, 'Green', grn, 0, 255, Math.round);
+    slider(gui, 'Blue', blu, 0, 255, Math.round);
+    panel.end(gui);
 
-    if (button(gui, 'add')) { mode = 'add'; }
-    if (button(gui, 'subtract')) { mode = 'sub'; }
-    if (button(gui, 'multiply')) { mode = 'mult'; }
-    if (button(gui, 'divide')) { mode = 'div'; }
-
-    let result: number;
-    if (mode === 'add') { result = numA + numB; }
-    if (mode === 'sub') { result = numA - numB; }
-    if (mode === 'mult') { result = numA * numB; }
-    if (mode === 'div') { result = numA / numB; }
-
-    editableText(gui, 'output', () => result.toString());
+    panel.begin(gui, 'Output', 350, 50, 75, 100);
+    gui.draw.setFillStyle(color.toHexString());
+    const { x, y, w, h } = gui.bounds.getElementBounds();
+    gui.draw.fillRect(x, y, w, h);
     panel.end(gui);
 
     gui.render();
